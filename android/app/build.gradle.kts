36335f0948a7c0aa87cf392e533d44d614b065c0
nothinking <pasioner@daum.net>
2025-07-15 11:00:00 +0900

    Update Android build configuration and add MainActivity class
    - Changed application ID and namespace to "com.seango.just_five_todos".
    - Implemented signing configuration for release builds using properties from key.properties.
    - Added MainActivity class for the Flutter application.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import java.util.Properties
+import java.io.FileInputStream
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -6,7 +9,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.just_five_todos"
+    namespace = "com.seango.just_five_todos"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.just_five_todos"
+        applicationId = "com.seango.just_five_todos"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -30,11 +33,30 @@ android {
         versionName = flutter.versionName
     }
 
+    // key.properties 파일 읽기
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = Properties()
+    if (keystorePropertiesFile.exists()) {
+        keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file(keystoreProperties["storeFile"] as String)
+            storePassword = keystoreProperties["storePassword"] as String
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+        }
+    }
+
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+        getByName("release") {
+            isMinifyEnabled = true // 필요시 true로 설정
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
         }
     }
 }
--- android/app/src/main/kotlin/com/seango/just_five_todos/MainActivity.kt
+++ android/app/src/main/kotlin/com/seango/just_five_todos/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.just_five_todos
+package com.seango.just_five_todos
 
 import io.flutter.embedding.android.FlutterActivity
 
